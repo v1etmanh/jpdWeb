b425b8d53cfc5554d46b9b4854897a7b39a07b0f
LE Viet Manh <mqnyle@gmail.com>
2025-09-13 09:02:05 +0700

    add first time

---
+++ src/main/java/com/jpd/web/JpDwebSwpApplication.java
@@ -0,0 +1,13 @@
+package com.jpd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpDwebSwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpDwebSwpApplication.class, args);
+	}
+
+}
